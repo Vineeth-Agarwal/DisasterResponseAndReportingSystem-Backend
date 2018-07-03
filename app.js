fa327bcc0630340bc57dfad0ceae60430c766c48
Kalburgi Srinivas <S530468@nwmissouri.edu>
2018-07-03 17:49:49 -0500

    eastabish database connection

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// cors
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// establish database
+require("./config/database")
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
