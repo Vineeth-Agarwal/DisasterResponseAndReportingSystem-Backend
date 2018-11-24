eb8751f1ad186d5d1d86f4642a28c67dbd03ff19
Chandra Mouli Kantipudi <chandramouli.kantipudi@gmail.com>
2018-11-24 16:20:13 -0600

    Added aler message for unsucessful login

--- views/src/app/auth/auth.service.ts
+++ views/src/app/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { User } from './user';
 
 @Injectable()
 export class AuthService {
+  public message = "Incorrect Usename or Passsword";
   private loggedIn = new BehaviorSubject<boolean>(false); // {1}
 
   get isLoggedIn() {
@@ -22,6 +23,11 @@ export class AuthService {
       this.loggedIn.next(true);
       this.router.navigate(['/dashboard']);
       console.log("Sucessfully logged in");
+      alert("Sucessfully logged in")
+    }
+    else{
+    console.log("Incorrect Usename or Passsword");
+     alert(this.message);
     }
   }
 
